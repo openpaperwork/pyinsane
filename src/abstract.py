aac870d193556552248e6220b55d25d407ff7387
Jerome Flesch <jflesch@gmail.com>
2012-05-27 15:05:29 +0200

    Abstract: Implements device finding + option getting and setting
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- src/abstract.py
+++ src/abstract.py
@@ -1,2 +1,119 @@
+import ctypes
 
+import rawapi
+
+_sane_instances = 0
+
+def _sane_init():
+    global _sane_instances
+    if _sane_instances == 0:
+        rawapi.sane_init()
+    _sane_instances += 1
+
+
+def _sane_exit():
+    global _sane_instances
+    _sane_instances -= 1
+    if _sane_instances == 0:
+        rawapi.sane_exit()
+
+
+class ScannerOption(object):
+    idx = 0
+    name = ""
+    title = ""
+    desc = ""
+    val_type = rawapi.SaneValueType(rawapi.SaneValueType.INT)
+    unit = rawapi.SaneUnit(rawapi.SaneUnit.NONE)
+    size = 4
+    capabilities = rawapi.SaneCapabilities(rawapi.SaneCapabilities.NONE)
+
+    def __init__(self, scanner, idx):
+        self.__scanner = scanner
+        self.idx = idx
+
+    @staticmethod
+    def build_from_rawapi(scanner, opt_idx, opt_raw):
+        opt = ScannerOption(scanner, opt_idx)
+        opt.name = opt_raw.name
+        opt.title = opt_raw.title
+        opt.desc = opt_raw.desc # TODO : multi-line
+        opt.val_type = rawapi.SaneValueType(opt_raw.type)
+        opt.unit = rawapi.SaneUnit(opt_raw.unit)
+        opt.size = opt_raw.size
+        opt.capabilities = rawapi.SaneCapabilities(opt_raw.cap)
+        return opt
+
+    def __get_value(self):
+        self.__scanner._open()
+        return rawapi.sane_get_option_value(self.__scanner._handle, self.idx)
+
+    def __set_value(self, new_value):
+        self.__scanner._open()
+        rawapi.sane_set_option_value(self.__scanner._handle, self.idx, new_value)
+
+    value = property(__get_value, __set_value)
+
+
+class Scanner(object):
+    def __init__(self, name, vendor="Unknown", model="Unknown",
+                 dev_type="Unknown"):
+        self.name = name
+        self.vendor = vendor
+        self.model = model
+        self.dev_type = dev_type
+        self._handle = None
+        self.__options = None  # { "name" : ScannerOption }
+
+    @staticmethod
+    def build_from_rawapi(sane_device):
+        return Scanner(sane_device.name, sane_device.vendor, sane_device.model,
+                       sane_device.type)
+
+    def _open(self):
+        if self._handle != None:
+            return
+        _sane_init()
+        self._handle = rawapi.sane_open(self.name)
+
+    def close(self):
+        if self._handle == None:
+            return
+        rawapi.sane_close(self._handle)
+        _sane_exit()
+
+    def __del__(self):
+        self.close()
+
+    def __load_options(self):
+        if self.__options != None:
+            return
+        self._open()
+        nb_options = rawapi.sane_get_option_value(self._handle, 0)
+        self.__options = {}
+        for opt_idx in range(1, nb_options):
+            opt_desc = rawapi.sane_get_option_descriptor(self._handle, opt_idx)
+            if not rawapi.SaneValueType(opt_desc.type).can_getset_opt():
+                continue
+            opt = ScannerOption.build_from_rawapi(self, opt_idx, opt_desc)
+            self.__options[opt.name] = opt
+
+    def __get_options(self):
+        self.__load_options()
+        return self.__options
+
+    options = property(__get_options)
+
+    def __str__(self):
+        return ("Scanner '%s' (%s, %s, %s)"
+                % (self.name, self.vendor, self.model, self.dev_type))
+
+
+def get_devices():
+    _sane_init()
+    try:
+        return [Scanner.build_from_rawapi(device)
+                for device in rawapi.sane_get_devices()]
+    finally:
+        _sane_exit()
 
--- tests/tests_abstract.py
+++ tests/tests_abstract.py
@@ -4,13 +4,59 @@ sys.path = ["src"] + sys.path
 import unittest
 
 import abstract
+import rawapi
+
+class TestSaneGetDevices(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_devices(self):
+        devices = abstract.get_devices()
+        self.assertTrue(len(devices) > 0)
+
+    def tearDown(self):
+        pass
+
+class TestSaneOptions(unittest.TestCase):
+    def setUp(self):
+        devices = abstract.get_devices()
+        self.assertTrue(len(devices) > 0)
+        self.dev = devices[0]
+
+    def test_get_option(self):
+        val = self.dev.options['mode'].value
+        self.assertNotEqual(val, None)
+
+    def test_set_option(self):
+        self.dev.options['mode'].value = "Gray"
+        val = self.dev.options['mode'].value
+        self.assertEqual(val, "Gray")
+
+    def __set_opt(self, opt_name, opt_val):
+        self.dev.options[opt_name].value = opt_val
+
+    def test_set_inexisting_option(self):
+        self.assertRaises(KeyError, self.__set_opt, 'xyz', "Gray")
+
+    def test_set_invalid_value(self):
+        self.assertRaises(rawapi.SaneException, self.__set_opt, 'mode', "XYZ")
+
+    def tearDown(self):
+        del(self.dev)
 
 
 def get_all_tests():
     all_tests = unittest.TestSuite()
 
-    tests = unittest.TestSuite([])
+    tests = unittest.TestSuite([TestSaneGetDevices("test_get_devices")])
+    all_tests.addTest(tests)
 
+    tests = unittest.TestSuite([
+        TestSaneOptions("test_get_option"),
+        TestSaneOptions("test_set_option"),
+        TestSaneOptions("test_set_inexisting_option"),
+        TestSaneOptions("test_set_invalid_value"),
+    ])
     all_tests.addTest(tests)
 
     return all_tests
