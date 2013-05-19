7c7f8d1f565b55b53f996d814317e09dc7b1ba52
Jerome Flesch <jflesch@gmail.com>
2013-05-19 19:05:10 +0200

    Setup.py : Add various informations for Pypi
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- setup.py
+++ setup.py
@@ -4,8 +4,25 @@ from setuptools import setup
 
 setup(name="pyinsane",
       version="1.0.1-git",
-      description=("Python implementation of the Sane API (using ctypes) and"
+      description=("Pure Python implementation of the Sane API (using ctypes) and"
                    " abstration layer"),
+      long_description=("Pure Python implementation of the Sane API (using"
+                        " ctypes). Include a thread-safe abstration layer"),
+      keywords="sane scanner",
+      url="https://github.com/jflesch/pyinsane",
+      download_url="https://github.com/jflesch/pyinsane/archive/v1.0.1.zip",
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 2 :: Only",
+          "Topic :: Multimedia :: Graphics :: Capture :: Scanners",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      license="GPLv3",
       author="Jerome Flesch",
       author_email="jflesch@gmail.com",
       packages=['pyinsane'],
