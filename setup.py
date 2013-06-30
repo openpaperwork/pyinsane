93ae64a7b9bfb63fe3d61c80b9b912bc4fadc761
Jerome Flesch <jflesch@gmail.com>
2013-06-30 21:42:06 +0200

    v1.0.3: - various fixes in test scripts list.py and scan.py - implements __eq__ on enums
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="pyinsane",
-      version="1.0.2-git",
+      version="1.0.3",
       description=("Pure Python implementation of the Sane API (using ctypes) and"
                    " abstration layer"),
       long_description=("Pure Python implementation of the Sane API (using"
