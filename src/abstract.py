741d26d71a83dbff2e55cf189b8f3048e8660da3
Jerome Flesch <jflesch@gmail.com>
2013-07-06 17:49:54 +0200

    Abstract: Fix scanning from ADF for scanners with a weird source naming convention
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- src/abstract.py
+++ src/abstract.py
@@ -301,7 +301,9 @@ class Scanner(object):
     options = property(_get_options)
 
     def scan(self, multiple=False):
-        if (not multiple) or self.options['source'].value != "ADF":
+        if (not multiple
+            or (not "ADF" in self.options['source'].value
+                and not "Feeder" in self.options['source'])):
             # XXX(Jflesch): We cannot use MultiScanSession() with something
             # else than an ADF. If we try, we will never get
             # SANE_STATUS_NO_DOCS from sane_start()/sane_read() and we will
