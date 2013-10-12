5ddac851c4c12aae6661496bbcb88bfa63dd25f3
Jerome Flesch <jflesch@gmail.com>
2013-10-12 20:15:40 +0200

    v1.3.3
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(name="pyinsane",
-      version="1.3.2-git",
+      version="1.3.3",
       description=("Pure Python implementation of the Sane API (using ctypes) and"
                    " abstration layer"),
       long_description=("Pure Python implementation of the Sane API (using"
                         " ctypes). Include a thread-safe abstraction layer"),
       keywords="sane scanner",
       url="https://github.com/jflesch/pyinsane",
-      download_url="https://github.com/jflesch/pyinsane/archive/v1.3.2.zip",
+          download_url="https://github.com/jflesch/pyinsane/archive/v1.3.3.zip",
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Intended Audience :: Developers",
