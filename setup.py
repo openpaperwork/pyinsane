66e291f3bb03024fb3288be57a8173d6f3e4e097
Jerome Flesch <jflesch@gmail.com>
2013-11-05 19:57:12 +0100

    v1.3.4 --> v1.3.4-git
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="pyinsane",
-      version="1.3.4",
+      version="1.3.4-git",
       description=("Pure Python implementation of the Sane API (using ctypes) and"
                    " abstration layer"),
       long_description=("Pure Python implementation of the Sane API (using"
