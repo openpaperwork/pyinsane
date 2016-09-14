7b31f3ac3a5ec723d1b503c17f9ec46a83553d98
Jerome Flesch <jflesch@gmail.com>
2016-09-14 22:26:42 +0200

    WIA/rawapi: add strings corresponding to each property values
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- src/wia/rawapi.cpp
+++ src/wia/rawapi.cpp
@@ -36,357 +36,366 @@ struct wia_source {
     IWiaItem2 *source;
 };
 
-static const int g_possible_connect_status[] = {
-    WIA_DEVICE_NOT_CONNECTED,
-    WIA_DEVICE_CONNECTED,
-    -1,
+struct wia_prop_int {
+    int value;
+    const char *name;
 };
 
-static const int g_possible_access_rights[] = {
-    WIA_ITEM_READ,
-    WIA_ITEM_WRITE,
-    WIA_ITEM_CAN_BE_DELETED,
-    WIA_ITEM_RD,
-    WIA_ITEM_RWD,
-    -1,
+struct wia_prop_clsid {
+    CLSID value;
+    const char *name;
 };
 
-static const int g_possible_compression[] = {
-    WIA_COMPRESSION_NONE,
-    WIA_COMPRESSION_AUTO,
-    WIA_COMPRESSION_BI_RLE4,
-    WIA_COMPRESSION_BI_RLE8,
-    WIA_COMPRESSION_G3,
-    WIA_COMPRESSION_G4,
-    WIA_COMPRESSION_JPEG,
-    WIA_COMPRESSION_JBIG,
-    WIA_COMPRESSION_JPEG2K,
-    WIA_COMPRESSION_PNG,
-    -1,
+static const struct wia_prop_int g_possible_connect_status[] = {
+    { WIA_DEVICE_NOT_CONNECTED, "not_connected" },
+    { WIA_DEVICE_CONNECTED, "connected" },
+    { -1, NULL, },
 };
 
-static const int g_possible_datatype[] = {
-    WIA_DATA_AUTO,
-    WIA_DATA_COLOR,
-    WIA_DATA_COLOR_DITHER,
-    WIA_DATA_COLOR_THRESHOLD,
-    WIA_DATA_DITHER,
-    WIA_DATA_GRAYSCALE,
-    WIA_DATA_THRESHOLD,
-    WIA_DATA_RAW_BGR,
-    WIA_DATA_RAW_CMY,
-    WIA_DATA_RAW_CMYK,
-    WIA_DATA_RAW_RGB,
-    WIA_DATA_RAW_YUV,
-    WIA_DATA_RAW_YUVK,
-    -1,
+static const struct wia_prop_int g_possible_access_rights[] = {
+    { WIA_ITEM_READ, "read" },
+    { WIA_ITEM_WRITE, "write" },
+    { WIA_ITEM_CAN_BE_DELETED, "can_be_deleted" },
+    { WIA_ITEM_RD, "read_can_be_deleted" },
+    { WIA_ITEM_RWD, "read_write_can_be_deleted" },
+    { -1, NULL, }
 };
 
-static const CLSID g_possible_format[] = {
-    WiaImgFmt_BMP,
-    WiaImgFmt_CIFF,
-    WiaImgFmt_EXIF,
-    WiaImgFmt_FLASHPIX,
-    WiaImgFmt_GIF,
-    WiaImgFmt_ICO,
-    WiaImgFmt_JBIG,
-    WiaImgFmt_JPEG,
-    WiaImgFmt_JPEG2K,
-    WiaImgFmt_JPEG2KX,
-    WiaImgFmt_MEMORYBMP,
-    WiaImgFmt_PDFA,
-    WiaImgFmt_PHOTOCD,
-    WiaImgFmt_PICT,
-    WiaImgFmt_PNG,
-    WiaImgFmt_RAW,
-    WiaImgFmt_RAWRGB,
-    WiaImgFmt_TIFF,
-    NULL,
+static const struct wia_prop_int g_possible_compression[] = {
+    { WIA_COMPRESSION_NONE, "none", },
+    { WIA_COMPRESSION_AUTO, "auto", },
+    { WIA_COMPRESSION_BI_RLE4, "bi_rle4", },
+    { WIA_COMPRESSION_BI_RLE8, "bi_rle8", },
+    { WIA_COMPRESSION_G3, "g3", },
+    { WIA_COMPRESSION_G4, "g4", },
+    { WIA_COMPRESSION_JPEG, "jpeg", },
+    { WIA_COMPRESSION_JBIG, "jbig", },
+    { WIA_COMPRESSION_JPEG2K, "jpeg2k", },
+    { WIA_COMPRESSION_PNG, "png", },
+    { -1, NULL, },
 };
 
-static const CLSID g_possible_item_category[] = {
-    WIA_CATEGORY_ROOT,
-    WIA_CATEGORY_FLATBED,
-    WIA_CATEGORY_FEEDER,
-    WIA_CATEGORY_FEEDER_FRONT,
-    WIA_CATEGORY_FEEDER_BACK,
-    WIA_CATEGORY_FILM,
-    WIA_CATEGORY_FOLDER,
-    WIA_CATEGORY_FINISHED_FILE,
-    NULL,
+static const struct wia_prop_int g_possible_datatype[] = {
+    { WIA_DATA_AUTO, "auto", },
+    { WIA_DATA_COLOR, "color", },
+    { WIA_DATA_COLOR_DITHER, "color_dither", },
+    { WIA_DATA_COLOR_THRESHOLD, "color_threshold", },
+    { WIA_DATA_DITHER, "dither", },
+    { WIA_DATA_GRAYSCALE, "grayscale", },
+    { WIA_DATA_THRESHOLD, "threshold", },
+    { WIA_DATA_RAW_BGR, "raw_bgr", },
+    { WIA_DATA_RAW_CMY, "raw_cmy", },
+    { WIA_DATA_RAW_CMYK, "raw_cmyk", },
+    { WIA_DATA_RAW_RGB, "raw_rgb", },
+    { WIA_DATA_RAW_YUV, "raw_yuv", },
+    { WIA_DATA_RAW_YUVK, "raw_yuvk", },
+    { -1, NULL, },
 };
 
-static const int g_possible_item_flags[] = {
-    WiaItemTypeAnalyze,
-    WiaItemTypeAudio,
-    WiaItemTypeBurst,
-    WiaItemTypeDeleted,
-    WiaItemTypeDocument,
-    WiaItemTypeDevice,
-    WiaItemTypeDisconnected,
-    WiaItemTypeFile,
-    WiaItemTypeFolder,
-    WiaItemTypeFree,
-    WiaItemTypeGenerated,
-    WiaItemTypeHasAttachments,
-    WiaItemTypeHPanorama,
-    WiaItemTypeImage,
-    WiaItemTypeProgrammableDataSource,
-    WiaItemTypeRoot,
-    WiaItemTypeStorage,
-    WiaItemTypeTransfer,
+static const struct wia_prop_clsid g_possible_format[] = {
+    { WiaImgFmt_BMP, "bmp", },
+    { WiaImgFmt_CIFF, "ciff", },
+    { WiaImgFmt_EXIF, "exif", },
+    { WiaImgFmt_FLASHPIX, "flashpix", },
+    { WiaImgFmt_GIF, "gif", },
+    { WiaImgFmt_ICO, "ico", },
+    { WiaImgFmt_JBIG, "jbig", },
+    { WiaImgFmt_JPEG, "jpeg", },
+    { WiaImgFmt_JPEG2K, "jpeg2k", },
+    { WiaImgFmt_JPEG2KX, "jpeg2kx", },
+    { WiaImgFmt_MEMORYBMP, "memorybmp", },
+    { WiaImgFmt_PDFA, "pdfa", },
+    { WiaImgFmt_PHOTOCD, "photocd", },
+    { WiaImgFmt_PICT, "pict", },
+    { WiaImgFmt_PNG, "png", },
+    { WiaImgFmt_RAW, "raw", },
+    { WiaImgFmt_RAWRGB, "rawrgb", },
+    { WiaImgFmt_TIFF, "tiff", },
+    { NULL, NULL, },
+};
+
+static const struct wia_prop_clsid g_possible_item_category[] = {
+    { WIA_CATEGORY_ROOT, "root", },
+    { WIA_CATEGORY_FLATBED, "flatbed", },
+    { WIA_CATEGORY_FEEDER, "feeder", },
+    { WIA_CATEGORY_FEEDER_FRONT, "feeder_front", },
+    { WIA_CATEGORY_FEEDER_BACK, "feeder_back", },
+    { WIA_CATEGORY_FILM, "film", },
+    { WIA_CATEGORY_FOLDER, "folder", },
+    { WIA_CATEGORY_FINISHED_FILE, "finished_file", },
+    { NULL, NULL, },
+};
+
+static const struct wia_prop_int g_possible_item_flags[] = {
+    { WiaItemTypeAnalyze, "analyze", },
+    { WiaItemTypeAudio, "audio", },
+    { WiaItemTypeBurst, "burst", },
+    { WiaItemTypeDeleted, "deleted", },
+    { WiaItemTypeDocument, "document", },
+    { WiaItemTypeDevice, "device", },
+    { WiaItemTypeDisconnected, "disconnected", },
+    { WiaItemTypeFile, "file", },
+    { WiaItemTypeFolder, "folder", },
+    { WiaItemTypeFree, "free", },
+    { WiaItemTypeGenerated, "generated", },
+    { WiaItemTypeHasAttachments, "has_attachments", },
+    { WiaItemTypeHPanorama, "hpanorama", },
+    { WiaItemTypeImage, "image", },
+    { WiaItemTypeProgrammableDataSource, "programmable_data_source", },
+    { WiaItemTypeRoot, "root", },
+    { WiaItemTypeStorage, "storage", },
+    { WiaItemTypeTransfer, "transfer", },
     // WiaItemTypeTwainCapabilityPassThrough, // Jflesch> Doesn't exist ?
-    WiaItemTypeVideo,
-    WiaItemTypeVPanorama,
-    -1,
+    { WiaItemTypeVideo, "video", },
+    { WiaItemTypeVPanorama, "vpanorama", },
+    { -1, NULL },
 };
 
-static const int g_possible_planar[] = {
-    WIA_PACKED_PIXEL,
-    WIA_PLANAR,
-    -1,
+static const struct wia_prop_int g_possible_planar[] = {
+    { WIA_PACKED_PIXEL, "pixel" },
+    { WIA_PLANAR, "planar" },
+    { -1, NULL, },
 };
 
-static const int g_possible_suppress_property_page[] = {
-    WIA_PROPPAGE_CAMERA_ITEM_GENERAL,
-    WIA_PROPPAGE_SCANNER_ITEM_GENERAL,
-    -1,
+static const struct wia_prop_int g_possible_suppress_property_page[] = {
+    { WIA_PROPPAGE_CAMERA_ITEM_GENERAL, "camera_item_general", },
+    { WIA_PROPPAGE_SCANNER_ITEM_GENERAL, "scanner_item_general", },
+    { -1, NULL, },
 };
 
-static const int g_possible_tymed[] = {
-    TYMED_CALLBACK,
-    TYMED_MULTIPAGE_CALLBACK,
-    TYMED_FILE,
-    TYMED_MULTIPAGE_FILE,
-    -1,
+static const struct wia_prop_int g_possible_tymed[] = {
+    { TYMED_CALLBACK, "callback", },
+    { TYMED_MULTIPAGE_CALLBACK, "multipage_callback", },
+    { TYMED_FILE, "file", },
+    { TYMED_MULTIPAGE_FILE, "multipage_file", },
+    { -1, NULL, },
 };
 
-static const int g_possible_dev_type[] = {
-    StiDeviceTypeDefault,
-    StiDeviceTypeScanner,
-    StiDeviceTypeDigitalCamera,
-    StiDeviceTypeStreamingVideo,
-    -1,
+static const struct wia_prop_int g_possible_dev_type[] = {
+    { StiDeviceTypeDefault, "default", },
+    { StiDeviceTypeScanner, "scanner", },
+    { StiDeviceTypeDigitalCamera, "digital_camera", },
+    { StiDeviceTypeStreamingVideo, "steaming_video", },
+    { -1, NULL, },
 };
 
-static const int g_possible_hw_config[] = {
-    1, // Generic WDM device
-    2, // SCSI device
-    4, // USB device
-    8, // Serial device
-    16, // Parallel device
-    -1,
+static const struct wia_prop_int g_possible_hw_config[] = {
+    { 1, "generic", }, // Generic WDM device
+    { 2, "scsi", }, // SCSI device
+    { 4, "usb", }, // USB device
+    { 8, "serial", }, // Serial device
+    { 16, "parallel", }, // Parallel device
+    { -1, NULL, },
 };
 
-static const int g_possible_document_handling_capabilities[] = {
-    AUTO_SOURCE,
-    ADVANCED_DUP,
-    DETECT_FILM_TPA,
-    DETECT_STOR,
-    FILM_TPA,
-    STOR,
-    DETECT_FEED,
-    DETECT_FLAT,
-    DETECT_SCAN,
-    DUP,
-    FEED,
-    FLAT,
-    DETECT_DUP,
-    DETECT_DUP_AVAIL,
-    DETECT_FEED_AVAIL,
-    -1,
+static const struct wia_prop_int g_possible_document_handling_capabilities[] = {
+    { AUTO_SOURCE, "auto_source", },
+    { ADVANCED_DUP, "dup", },
+    { DETECT_FILM_TPA, "detect_film_tpa", },
+    { DETECT_STOR, "detect_stor", },
+    { FILM_TPA, "film_tpa", },
+    { STOR, "stor", },
+    { DETECT_FEED, "detect_feed", },
+    { DETECT_FLAT, "detect_flat", },
+    { DETECT_SCAN, "detect_scan", },
+    { DUP, "dup", },
+    { FEED, "feed", },
+    { FLAT, "flat", },
+    { DETECT_DUP, "detect_dup", },
+    { DETECT_DUP_AVAIL, "detect_dup_avail", },
+    { DETECT_FEED_AVAIL, "detect_feed_avail", },
+    { -1, NULL, },
 };
 
-static const int g_possible_document_handling_select[] = {
-    FEEDER,
-    FLATBED,
-    DUPLEX,
-    AUTO_ADVANCE,
-    FRONT_FIRST,
-    BACK_FIRST,
-    FRONT_ONLY,
-    BACK_ONLY,
-    NEXT_PAGE,
-    PREFEED,
-    -1,
+static const struct wia_prop_int g_possible_document_handling_select[] = {
+    { FEEDER, "feeder", },
+    { FLATBED, "flatbed", },
+    { DUPLEX, "duplex", },
+    { AUTO_ADVANCE, "auto_advance", },
+    { FRONT_FIRST, "front_first", },
+    { BACK_FIRST, "back_first", },
+    { FRONT_ONLY, "front_only", },
+    { BACK_ONLY, "back_only", },
+    { NEXT_PAGE, "next_page", },
+    { PREFEED, "prefeed", },
+    { -1, NULL, },
 };
 
-static const int g_possible_document_handling_status[] = {
-    FEED_READY,
-    FLAT_READY,
-    DUP_READY,
-    FLAT_COVER_UP,
-    PATH_COVER_UP,
-    PAPER_JAM,
-    FILM_TPA_READY,
-    STORAGE_READY,
-    STORAGE_FULL,
-    MULTIPLE_FEED,
-    DEVICE_ATTENTION,
-    LAMP_ERR,
-    -1,
+static const struct wia_prop_int g_possible_document_handling_status[] = {
+    { FEED_READY, "feed_ready", },
+    { FLAT_READY, "flat_ready", },
+    { DUP_READY, "dup_ready", },
+    { FLAT_COVER_UP, "flat_cover_up", },
+    { PATH_COVER_UP, "path_cover_up", },
+    { PAPER_JAM, "paper_jam", },
+    { FILM_TPA_READY, "film_tpa_ready", },
+    { STORAGE_READY, "storage_ready", },
+    { STORAGE_FULL, "storage_full", },
+    { MULTIPLE_FEED, "multiple_feed", },
+    { DEVICE_ATTENTION, "device_attention", },
+    { LAMP_ERR, "lamp_err", },
+    { -1, NULL, },
 };
 
-static const int g_possible_horizontal_bed_registration[] = {
-    LEFT_JUSTIFIED,
-    CENTERED,
-    RIGHT_JUSTIFIED,
-    -1,
+static const struct wia_prop_int g_possible_horizontal_bed_registration[] = {
+    { LEFT_JUSTIFIED, "left_justified", },
+    { CENTERED, "centered", },
+    { RIGHT_JUSTIFIED, "right_justified", },
+    { -1, NULL, },
 };
 
-static const int g_possible_orientation[] = {
-    LANDSCAPE,
-    PORTRAIT,
-    ROT180,
-    ROT270,
-    -1,
+static const struct wia_prop_int g_possible_orientation[] = {
+    { LANDSCAPE, "landscape", },
+    { PORTRAIT, "portrait", },
+    { ROT180, "rot180", },
+    { ROT270, "rot270", },
+    { -1, NULL },
 };
 
-static const int g_possible_page_size[] = {
-    WIA_PAGE_A4,
-    WIA_PAGE_CUSTOM, // see WIA_DPS_PAGE_HEIGHT and WIA_DPS_PAGE_WIDTH
-    WIA_PAGE_LETTER,
-    WIA_PAGE_USLEGAL,
-    WIA_PAGE_USLETTER,
-    WIA_PAGE_USLEDGER,
-    WIA_PAGE_USSTATEMENT,
-    WIA_PAGE_BUSINESSCARD,
-    WIA_PAGE_ISO_A0,
-    WIA_PAGE_ISO_A1,
-    WIA_PAGE_ISO_A2,
-    WIA_PAGE_ISO_A3,
-    WIA_PAGE_ISO_A4,
-    WIA_PAGE_ISO_A5,
-    WIA_PAGE_ISO_A6,
-    WIA_PAGE_ISO_A7,
-    WIA_PAGE_ISO_A8,
-    WIA_PAGE_ISO_A9,
-    WIA_PAGE_ISO_A10,
-    WIA_PAGE_ISO_B0,
-    WIA_PAGE_ISO_B1,
-    WIA_PAGE_ISO_B2,
-    WIA_PAGE_ISO_B3,
-    WIA_PAGE_ISO_B4,
-    WIA_PAGE_ISO_B5,
-    WIA_PAGE_ISO_B6,
-    WIA_PAGE_ISO_B7,
-    WIA_PAGE_ISO_B8,
-    WIA_PAGE_ISO_B9,
-    WIA_PAGE_ISO_B10,
-    WIA_PAGE_ISO_C0,
-    WIA_PAGE_ISO_C1,
-    WIA_PAGE_ISO_C2,
-    WIA_PAGE_ISO_C3,
-    WIA_PAGE_ISO_C4,
-    WIA_PAGE_ISO_C5,
-    WIA_PAGE_ISO_C6,
-    WIA_PAGE_ISO_C7,
-    WIA_PAGE_ISO_C8,
-    WIA_PAGE_ISO_C9,
-    WIA_PAGE_ISO_C10,
-    WIA_PAGE_JIS_B0,
-    WIA_PAGE_JIS_B1,
-    WIA_PAGE_JIS_B2,
-    WIA_PAGE_JIS_B3,
-    WIA_PAGE_JIS_B4,
-    WIA_PAGE_JIS_B5,
-    WIA_PAGE_JIS_B6,
-    WIA_PAGE_JIS_B7,
-    WIA_PAGE_JIS_B8,
-    WIA_PAGE_JIS_B9,
-    WIA_PAGE_JIS_B10,
-    WIA_PAGE_JIS_2A,
-    WIA_PAGE_JIS_4A,
-    WIA_PAGE_DIN_2B,
-    WIA_PAGE_DIN_2B,
-    WIA_PAGE_DIN_4B,
-    WIA_PAGE_AUTO,
-    WIA_PAGE_CUSTOM_BASE,
-    -1,
+static const struct wia_prop_int g_possible_page_size[] = {
+    { WIA_PAGE_A4, "a4", },
+    { WIA_PAGE_CUSTOM, "custom", }, // see WIA_DPS_PAGE_HEIGHT and WIA_DPS_PAGE_WIDTH
+    { WIA_PAGE_LETTER, "letter", },
+    { WIA_PAGE_USLEGAL, "uslegal", },
+    { WIA_PAGE_USLETTER, "usletter", },
+    { WIA_PAGE_USLEDGER, "usledger", },
+    { WIA_PAGE_USSTATEMENT, "usstatement", },
+    { WIA_PAGE_BUSINESSCARD, "businesscard", },
+    { WIA_PAGE_ISO_A0, "iso_a0", },
+    { WIA_PAGE_ISO_A1, "iso_a1", },
+    { WIA_PAGE_ISO_A2, "iso_a2", },
+    { WIA_PAGE_ISO_A3, "iso_a3", },
+    { WIA_PAGE_ISO_A4, "iso_a4", },
+    { WIA_PAGE_ISO_A5, "iso_a5", },
+    { WIA_PAGE_ISO_A6, "iso_a6", },
+    { WIA_PAGE_ISO_A7, "iso_a7", },
+    { WIA_PAGE_ISO_A8, "iso_a8", },
+    { WIA_PAGE_ISO_A9, "iso_a9", },
+    { WIA_PAGE_ISO_A10, "iso_a10", },
+    { WIA_PAGE_ISO_B0, "iso_b0", },
+    { WIA_PAGE_ISO_B1, "iso_b1", },
+    { WIA_PAGE_ISO_B2, "iso_b2", },
+    { WIA_PAGE_ISO_B3, "iso_b3", },
+    { WIA_PAGE_ISO_B4, "iso_b4", },
+    { WIA_PAGE_ISO_B5, "iso_b5", },
+    { WIA_PAGE_ISO_B6, "iso_b6", },
+    { WIA_PAGE_ISO_B7, "iso_b7", },
+    { WIA_PAGE_ISO_B8, "iso_b8", },
+    { WIA_PAGE_ISO_B9, "iso_b9", },
+    { WIA_PAGE_ISO_B10, "iso_b10", },
+    { WIA_PAGE_ISO_C0, "iso_c0", },
+    { WIA_PAGE_ISO_C1, "iso_c1", },
+    { WIA_PAGE_ISO_C2, "iso_c2", },
+    { WIA_PAGE_ISO_C3, "iso_c3", },
+    { WIA_PAGE_ISO_C4, "iso_c4", },
+    { WIA_PAGE_ISO_C5, "iso_c5", },
+    { WIA_PAGE_ISO_C6, "iso_c6", },
+    { WIA_PAGE_ISO_C7, "iso_c7", },
+    { WIA_PAGE_ISO_C8, "iso_c8", },
+    { WIA_PAGE_ISO_C9, "iso_c9", },
+    { WIA_PAGE_ISO_C10, "iso_c10", },
+    { WIA_PAGE_JIS_B0, "jis_b0", },
+    { WIA_PAGE_JIS_B1, "jis_b1", },
+    { WIA_PAGE_JIS_B2, "jis_b2", },
+    { WIA_PAGE_JIS_B3, "jis_b3", },
+    { WIA_PAGE_JIS_B4, "jis_b4", },
+    { WIA_PAGE_JIS_B5, "jis_b5", },
+    { WIA_PAGE_JIS_B6, "jis_b6", },
+    { WIA_PAGE_JIS_B7, "jis_b7", },
+    { WIA_PAGE_JIS_B8, "jis_b8", },
+    { WIA_PAGE_JIS_B9, "jis_b9", },
+    { WIA_PAGE_JIS_B10, "jis_b10", },
+    { WIA_PAGE_JIS_2A, "jis_2a", },
+    { WIA_PAGE_JIS_4A, "jis_4a", },
+    { WIA_PAGE_DIN_2B, "din_2b", },
+    { WIA_PAGE_DIN_4B, "din_4b", },
+    { WIA_PAGE_AUTO, "auto", },
+    { WIA_PAGE_CUSTOM_BASE, "custom_base", },
+    { -1, NULL, },
 };
 
-static const int g_possible_preview[] = {
-    WIA_FINAL_SCAN,
-    WIA_PREVIEW_SCAN,
-    -1,
+static const struct wia_prop_int g_possible_preview[] = {
+    { WIA_FINAL_SCAN, "final_scan", },
+    { WIA_PREVIEW_SCAN, "preview_scan", },
+    { -1, NULL, },
 };
 
-static const int g_possible_sheet_feeder_registration[] = {
-    LEFT_JUSTIFIED,
-    CENTERED,
-    RIGHT_JUSTIFIED,
-    -1,
+static const struct wia_prop_int g_possible_sheet_feeder_registration[] = {
+    { LEFT_JUSTIFIED, "left_justified", },
+    { CENTERED, "centered", },
+    { RIGHT_JUSTIFIED, "right_justified", },
+    { -1, NULL },
 };
 
-static const int g_possible_show_preview_control[] = {
-    WIA_SHOW_PREVIEW_CONTROL,
-    WIA_DONT_SHOW_PREVIEW_CONTROL,
-    -1,
+static const struct wia_prop_int g_possible_show_preview_control[] = {
+    { WIA_SHOW_PREVIEW_CONTROL, "show_preview_control", },
+    { WIA_DONT_SHOW_PREVIEW_CONTROL, "dont_show_preview_control", },
+    { -1, NULL, },
 };
 
-static const int g_possible_vertical_bed_registration[] = {
-    TOP_JUSTIFIED,
-    CENTERED,
-    BOTTOM_JUSTIFIED,
-    -1,
+static const struct wia_prop_int g_possible_vertical_bed_registration[] = {
+    { TOP_JUSTIFIED, "top_justified", },
+    { CENTERED, "centered", },
+    { BOTTOM_JUSTIFIED, "bottom_justified", },
+    { -1, NULL, },
 };
 
-static const int g_possible_auto_deskew[] = {
-    WIA_AUTO_DESKEW_ON,
-    WIA_AUTO_DESKEW_OFF,
-    -1,
+static const struct wia_prop_int g_possible_auto_deskew[] = {
+    { WIA_AUTO_DESKEW_ON, "deskew_on", },
+    { WIA_AUTO_DESKEW_OFF, "deskew_off", },
+    { -1, NULL, },
 };
 
-static const int g_possible_intent[] = {
-    WIA_INTENT_NONE,
-    WIA_INTENT_IMAGE_TYPE_COLOR,
-    WIA_INTENT_IMAGE_TYPE_GRAYSCALE,
-    WIA_INTENT_IMAGE_TYPE_TEXT,
-    WIA_INTENT_IMAGE_TYPE_MASK,
-    WIA_INTENT_MINIMIZE_SIZE,
-    WIA_INTENT_MAXIMIZE_QUALITY,
-    WIA_INTENT_SIZE_MASK,
-    WIA_INTENT_BEST_PREVIEW,
-    -1,
+static const struct wia_prop_int g_possible_intent[] = {
+    { WIA_INTENT_NONE, "none", },
+    { WIA_INTENT_IMAGE_TYPE_COLOR, "image_type_color", },
+    { WIA_INTENT_IMAGE_TYPE_GRAYSCALE, "image_type_grayscale", },
+    { WIA_INTENT_IMAGE_TYPE_TEXT, "image_type_text", },
+    { WIA_INTENT_IMAGE_TYPE_MASK, "image_type_mask", },
+    { WIA_INTENT_MINIMIZE_SIZE, "minimize_size", },
+    { WIA_INTENT_MAXIMIZE_QUALITY, "maximize_quality", },
+    { WIA_INTENT_SIZE_MASK, "size_mask", },
+    { WIA_INTENT_BEST_PREVIEW, "best_preview", },
+    { -1, NULL, },
 };
 
-static const int g_possible_film_scan_mode[] = {
-    WIA_FILM_COLOR_SLIDE,
-    WIA_FILM_COLOR_NEGATIVE,
-    WIA_FILM_BW_NEGATIVE,
-    -1,
+static const struct wia_prop_int g_possible_film_scan_mode[] = {
+    { WIA_FILM_COLOR_SLIDE, "color_slide", },
+    { WIA_FILM_COLOR_NEGATIVE, "color_negative", },
+    { WIA_FILM_BW_NEGATIVE, "bw_negative", },
+    { -1, NULL, },
 };
 
-static const int g_possible_lamp[] = {
-    WIA_LAMP_ON,
-    WIA_LAMP_OFF,
-    -1,
+static const struct wia_prop_int g_possible_lamp[] = {
+    { WIA_LAMP_ON, "on", },
+    { WIA_LAMP_OFF, "off", },
+    { -1, NULL, },
 };
 
-static const int g_possible_photometric_interp[] = {
-    WIA_PHOTO_WHITE_0, // WHITE is 0, and BLACK is 1
-    WIA_PHOTO_WHITE_1, // WHITE is 1, and BLACK is 0
-    -1,
+static const struct wia_prop_int g_possible_photometric_interp[] = {
+    { WIA_PHOTO_WHITE_0, "white_0", }, // WHITE is 0, and BLACK is 1
+    { WIA_PHOTO_WHITE_1, "white_1", }, // WHITE is 1, and BLACK is 0
+    { -1, NULL, },
 };
 
-static const int g_possible_preview_type[] = {
-    WIA_ADVANCED_PREVIEW,
-    WIA_BASIC_PREVIEW,
-    -1,
+static const struct wia_prop_int g_possible_preview_type[] = {
+    { WIA_ADVANCED_PREVIEW, "advanced", },
+    { WIA_BASIC_PREVIEW, "basic", },
+    { -1, NULL, },
 };
 
-static const int g_possible_rotation[] = {
-    PORTRAIT,
-    LANDSCAPE,
-    ROT180,
-    ROT270,
-    -1,
+static const struct wia_prop_int g_possible_rotation[] = {
+    { PORTRAIT, "portrait", },
+    { LANDSCAPE, "landscape", },
+    { ROT180, "rot180", },
+    { ROT270, "rot270", },
+    { -1, NULL, },
 };
 
-static const int g_possible_segmentation[] = {
-    WIA_USE_SEGMENTATION_FILTER,
-    WIA_DONT_USE_SEGMENTATION_FILTER,
-    -1,
+static const struct wia_prop_int g_possible_segmentation[] = {
+    { WIA_USE_SEGMENTATION_FILTER, "true", },
+    { WIA_DONT_USE_SEGMENTATION_FILTER, "false", },
+    { -1, NULL, },
 };
 
 struct wia_property {
@@ -888,7 +897,7 @@ static PyObject *get_devices(PyObject *, PyObject* args)
 
         hr = get_device_basic_infos(properties, &dev_infos);
         if (FAILED(hr)) {
-            Py_RETURN_NONE;
+            break;
         }
         if (dev_infos == NULL) {
             // not a scanner
