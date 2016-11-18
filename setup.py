7198ce753a0d8f809a3692c5804e34969e7458ea
Jerome Flesch <jflesch@openpaper.work>
2016-11-18 16:14:26 +0100

    2.0.4
    Signed-off-by: Jerome Flesch <jflesch@openpaper.work>

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name="pyinsane2",
-    version="2.0.3",
+    version="2.0.4",
     description=(
         "Python library to access and use image scanners (Linux/Windows/etc)"
     ),
